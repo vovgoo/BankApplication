54da7b93c57080709cd2375247f20a7f9ee95703
vovgoo <vov.gonchar2017@yandex.ru>
2023-12-15 18:48:04 +0300

    Add Login and Register scene

--- src/main/java/com/example/demo3/HelloApplication.java
+++ src/main/java/com/example/demo3/HelloApplication.java
@@ -6,13 +6,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main_window.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/style.css")).toExternalForm());
+        stage.setTitle("Bank Application!");
         stage.setScene(scene);
         stage.show();
     }
